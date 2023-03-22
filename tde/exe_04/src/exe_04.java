240478cfd50af917e6920a7f7c15b592f19a0f0c
Gabriel Baltazar <112046061+gabrielbbalttazar@users.noreply.github.com>
2023-03-22 19:41:48 -0300

    Alterações
    Envio com alteração

--- tde/exe_04/src/exe_04.java
+++ tde/exe_04/src/exe_04.java
@@ -1,10 +1,8 @@
 public class exe_04 {
     public static void main(String[] args) {
 
-        int numero = 101;
+        int numero = 100;
         int contador = 1;
-
-        System.out.println(1);
 
         while (contador <= numero) {
             if (contador % 2 == 0) {
@@ -12,10 +10,6 @@ public class exe_04 {
             }
             contador++;
         }
-        if (numero % 2 == 0) {
 
-        } else {
-            System.out.println(numero);
-        }
     }
 }
--- tde/exe_05/src/exe_05.java
+++ tde/exe_05/src/exe_05.java
@@ -1,20 +1,14 @@
 public class exe_05 {
     public static void main(String[] args) {
 
-        int numero = 505;
+        int numero = 506;
 
         for (int contador = 1; contador <= numero; contador++) {
-            if (contador % 2 != 0 || contador == 1) {
-                if (contador == 1) {
-                    System.out.println(contador);
-                } else {
-                    System.out.println(contador);
-                }
+            if (contador % 2 != 0) {
+                System.out.println(contador);
             }
         }
-        if (numero % 2 == 0) {
-            System.out.println(numero);
-        }
+
     }
 }
 
